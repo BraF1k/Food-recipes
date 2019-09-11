90b46c42db2b9bc8420118aeffa862ecebfe9325
Serhii Buriak <sergeyburiak44@gmail.com>
2019-09-11 23:02:49 +0300

    Fixed litle bug with link to ShowBox

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,24 @@
 import {
-    ShowRecipe, HideRecipe
+    ShowRecipe,
+    HideRecipe
 } from "./show-recipe";
-import { PostRecipe } from "./post";
+import {
+    PostRecipe
+} from "./post";
 
-document.querySelector('.right-recipes-block').addEventListener('click', function (event) {
+
+
+document.querySelector('.wraper-right').addEventListener('click', function (event) {
     ShowRecipe(event.target)
 })
 
-document.querySelector('.full-recipe-block_close-btn').addEventListener('click', function() {
+
+
+document.querySelector('.full-recipe-block_close-btn').addEventListener('click', function () {
     HideRecipe();
 });
 
-document.querySelector('.overlay').addEventListener('click', function() {
+document.querySelector('.overlay').addEventListener('click', function () {
     HideRecipe();
 });
 
